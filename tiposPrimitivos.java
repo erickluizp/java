b9aa78a4ad79d85a88642c9b866434edb4c6240d
erickluizp <elxfp14@gmail.com>
2025-05-13 15:49:20 -0300

    Create tiposPrimitivos.java
    Usando alguns tipos primitivos para imprimir uma frase.

---
+++ tiposPrimitivos.java
@@ -0,0 +1,13 @@
+public class tiposPrimitivos {
+    public static void main(String[] args) {
+
+        String nome = "Robert";
+        String endereco = "Rua Sales de Lima";
+        double salario = 2500.00;
+        String dataRecebimentoSalario = "20/10/2025";
+        short idade = 17;
+        String relatorio = "Eu "+nome+", morando no endereço "+endereco+", tendo" +idade+ " anos, confirmo que recebi o salário de "+salario+" na data "+dataRecebimentoSalario;
+
+        System.out.println(relatorio);
+    }
+}
